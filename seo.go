b1b5111ef2cf7ff381c180f9425765bd6047e8fb
Gabor Szabo <gabor@szabgab.com>
2020-07-02 00:37:13 +0300

    start writing the HTML parser

--- seo.go
+++ seo.go
@@ -3,10 +3,73 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/net/html"
 )
+
+func processURL(currentURL string) {
+	resp, err := http.Get(currentURL)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	// fmt.Println(resp)
+	//fmt.Println(resp.StatusCode)
+	if resp.StatusCode != 200 {
+		log.Printf("Status code is %v", resp.StatusCode)
+		return
+	}
+	// fmt.Println(resp.Header)
+	//fmt.Printf("%T", resp)
+	defer resp.Body.Close()
+	//fmt.Printf("%T\n", resp.Body)
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Printf("Could not read body: %v", err)
+	// 	return
+	// }
+	// body = body
+	// fmt.Println(string(body))
+	// txt := "<html></html>"
+	zz := html.NewTokenizer(resp.Body)
+	for {
+		tt := zz.Next()
+		if tt == html.ErrorToken {
+			if zz.Err() == io.EOF {
+				return
+			}
+			log.Printf("Error: %v", zz.Err())
+			return
+		}
+		//fmt.Printf("Token: %v\n", zz.Token())
+		tag, _ := zz.TagName()
+		//fmt.Print(x)
+		// if err != nil {
+		// 	log.Printf("Error: %v", err)
+		// 	continue
+		// }
+		fmt.Printf("Tag: %v\n", string(tag))
+		for {
+			attrKey, attrValue, moreAttr := zz.TagAttr()
+			if string(attrKey) == "" {
+				break
+			}
+			fmt.Printf("Attr: %v\n", string(attrKey))
+			fmt.Printf("Attr: %v\n", string(attrValue))
+			fmt.Printf("Attr: %v\n", moreAttr)
+			if !moreAttr {
+				break
+			}
+		}
+		// Process the current token.
+	}
+
+	return
+}
 
 func main() {
 	var host string
@@ -22,16 +85,5 @@ func main() {
 	// TODO: stop on Nth error?
 	log.Printf("Host: %v", host)
 
-	resp, err := http.Get(host)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-	// fmt.Println(resp)
-	//fmt.Println(resp.StatusCode)
-	if resp.StatusCode != 200 {
-		log.Printf("Status code is %v", resp.StatusCode)
-	}
-	// fmt.Println(resp.Header)
-	//fmt.Printf("%T", resp)
+	processURL(host)
 }
